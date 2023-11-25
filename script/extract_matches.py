1b54268a05b35c8e60b958dd4027cb00b4fdd8ce
izzatarroyyan12 <izzatarroyyan12@gmail.com>
2023-11-25 19:04:18 +0700

    feat: "Add logging for extraction script"

--- script/extract_matches.py
+++ script/extract_matches.py
@@ -1,44 +1,58 @@
-# extract_api_matches.py
-
 import requests
 import pandas as pd
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 def make_api_request(url, headers, params):
-    response = requests.get(url, headers=headers, params=params)
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        logging.error(f"HTTP Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        logging.error(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        logging.error(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        logging.error(f"Something went wrong: {err}")
     return response
 
 def extract_match_data(matches):
-    match_data = []
+    try:
+        match_data = []
+        for match in matches:
+            match_id = match['id']
+            matchday = match['matchday']
+            home_team = match['homeTeam']['name']
+            home_team_id = match['homeTeam']['id']
+            away_team = match['awayTeam']['name']
+            away_team_id = match['awayTeam']['id']
+            home_score = match['score']['fullTime']['home']
+            away_score = match['score']['fullTime']['away']
+            played = home_score is not None and away_score is not None
 
-    for match in matches:
-        match_id = match['id']
-        matchday = match['matchday']
-        home_team = match['homeTeam']['name']
-        home_team_id = match['homeTeam']['id']
-        away_team = match['awayTeam']['name']
-        away_team_id = match['awayTeam']['id']
-        home_score = match['score']['fullTime']['home']
-        away_score = match['score']['fullTime']['away']
-        played = home_score is not None and away_score is not None
-
-        match_data.append({
-            'matchId': match_id,
-            'matchday': matchday,
-            'homeTeam': home_team,
-            'homeTeamId': home_team_id,
-            'awayTeam': away_team,
-            'awayTeamId': away_team_id,
-            'homeScore': home_score,
-            'awayScore': away_score,
-            'played': played
-        })
-
+            match_data.append({
+                'matchId': match_id,
+                'matchday': matchday,
+                'homeTeam': home_team,
+                'homeTeamId': home_team_id,
+                'awayTeam': away_team,
+                'awayTeamId': away_team_id,
+                'homeScore': home_score,
+                'awayScore': away_score,
+                'played': played
+            })
+    except Exception as e:
+        logging.error(f"Error occurred while extracting match data: {e}")
     return match_data
 
 def export_to_csv(dataframe, csv_filename):
-    dataframe.to_csv(csv_filename, index=False)
-    logging.info(f"Data exported to {csv_filename} successfully.")
+    try:
+        dataframe.to_csv(csv_filename, index=False)
+        logging.info(f"Data exported to {csv_filename} successfully.")
+    except Exception as e:
+        logging.error(f"Error occurred while exporting data to CSV: {e}")
 
 def extract_api_matches(api_key):
     url = 'https://api.football-data.org/v4/competitions/PL/matches'
@@ -49,7 +63,6 @@ def extract_api_matches(api_key):
 
     if response.status_code == 200:
         data = response.json()
-        logging.info(data)
 
         # Extract match data
         matches = data.get('matches', [])
